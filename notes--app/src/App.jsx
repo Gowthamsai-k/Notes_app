e6cc51a0bbc51207efdf9b71df480d0c1c32dbc9
Gowtham <gowthamsai0519@gmail.com>
2026-09-02 12:48:28 +0000

    learnt basiics

--- notes--app/src/App.jsx
+++ notes--app/src/App.jsx
@@ -1,51 +1,54 @@
-import { useState } from "react";
-import  axios  from 'axios'
+import {useState } from 'react'
+
 export default function App()
 {
+
+  const [tasks , setTasks] = useState([])
   const [form , setForm] = useState({
-    task : "",
-    description: "" ,
-    active : true
+    id :Date.now(),
+    task : "", 
+    desc : ""
   })
 
   const handleChange = (e) =>{
+    setForm({...form , [e.target.name] : e.target.value})
+  }
+
+  const handleSubmit = (e) =>{
+    e.preventDefault()
+    setTasks([...tasks , form])
     setForm({
-      ...form , [e.target.name] : e.target.value
+      id : Date.now(),
+      task : "",
+      desc : ""
     })
   }
-  const handleSumbit = (e) =>{
+  const handleDelete = (id) =>{
+    setTasks(tasks.filter((item) => item.id != id))
 
-    e.preventDefault()
-    console.log(form.task);
-    console.log(form.description)
-    axios.post('https://symmetrical-succotash-jj4wjp497r94cv7g-3000.app.github.dev/save',form
-    )  .then(response => console.log(response.data))
-  .catch(error => console.error(error))
   }
+
   return(
     <>
-    <h1>Notes App</h1>
-    <form onSubmit={handleSumbit}>
-      <label>Enter task</label>
-
-      <input type = "text" 
-      onChange ={handleChange}
-      name = "task" 
-      value = {form.task}>
-       </input>
-
+    <form onSubmit = {handleSubmit} >
+      <label>Enter the task</label>
+      <input type= "text" name = "task" value = {form.task} required = "true" onChange = {handleChange}></input>
       <br />
-      <label>Enter description of the task</label>
-
-      <input type = "text" 
-      onChange = {handleChange} 
-      name = "description" 
-      value = {form.description}>
-       </input>
+      <label>enter the task desc</label>
+      <input type = "text" name = "desc" value = {form.desc} required = "true" onChange = {handleChange}></input>
       <br />
-
-      <button type = "submit">Add Task</button>
+      <button type = "submit" >Add</button>
     </form>
+    <div>
+      {tasks.map((item , index) => (
+        <ol key = {index}>
+          <li>{item.task}</li>
+          <li>{item.desc}</li>
+          <button onClick = {() => {handleDelete(item.id)}}>Delete</button>
+        </ol>
+      ))}
+      
+      </div>
     </>
   )
 }
