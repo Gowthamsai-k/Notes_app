dc963915f6c0eec8068aca91e58bc69835d04482
Gowtham <gowthamsai0519@gmail.com>
2026-09-04 13:35:26 +0000

    learnt node

--- index.js
+++ index.js
@@ -1,18 +1,52 @@
-import express from "express"
+import { MongoClient } from 'mongodb';
+import cors from 'cors'
+async function list_database(){
+    
+    const uri = ""
+ 
 
+    const client = new MongoClient(uri);
+ 
+    try {
+    
+        await client.connect();
+ 
+        
+        const res =   await client
+        .db()
+        .admin()
+        .listDatabases();
+        return res.databases;
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
+
+import express from 'express' 
 const app = express()
+
 app.use(cors());
 app.use(express.json());
-app.get("/" , (req , res) =>{
-    res.send({"status" : "Healthy"})
-})
 
-app.post("/save" , (req , res) =>{
-    const {task , description , active} = req.body
-    console.log(task , description , active)
+app.get("/" , ((req , res) =>(
+    res.send("hello world")
+)))
 
-})
-
-app.listen(3000 , () =>{
-    console.log("server is running on port 3000")
-})
+app.post("/tasks" , ((req , res) =>{
+    const data = req.body;
+    console.log(data);
+    res.status(201).json({
+        message: "data received successfully",
+        data: data
+    });
+}))
+app.get("/db" ,  (async (req , res) =>{
+    const database = await list_database().catch(console.error);
+    res.send(database)
+    
+}))
+app.listen(3000, "0.0.0.0", () => {
+    console.log("Server running on port 3000");
+});
--- notes--app/src/App.jsx
+++ notes--app/src/App.jsx
@@ -1,17 +1,65 @@
-import {useState } from 'react'
-import Inventory from './components/Inventory.jsx'
-import {createContext} from 'react';
-import Shop from './components/Shop'
+import {useState} from 'react';
+import axios from 'axios';
 
-export const medContext = createContext(); 
 export default function App()
 {
-  const [med , setMed] = useState([])
+  const [form , setForm] = useState({
+    name : "",
+    desc : ""
+  });
+  const [task , setTask] = useState([])
 
-  return(
-    <medContext.Provider value = {{med , setMed}}>
-    <Inventory />
-    <Shop />
-    </ medContext.Provider >
-  )
+  const handleChange = (e) =>{
+    setForm({...form , [e.target.name] : e.target.value})
+  }
+
+  const  handleSubmit = async (e) =>{
+    e.preventDefault()
+    try{
+      const res = await axios.post("https://bookish-orbit-q79g759649j9344q5-3000.app.github.dev/tasks" , 
+        form)
+      console.log(res.data);
+    }
+    catch (e)
+    {
+      console.log("error making the call" , e);
+    }
+    setTask([...task , form])
+    setForm({
+      name : "" ,
+      desc : ""
+    })
+
+  }
+    return(
+      <>
+      <h1>Notes app</h1>
+      <form onSubmit = {handleSubmit}>
+        <label>Enter the task</label>
+        <br />
+        <input type = "text" name = "name" value = {form.name} onChange = {handleChange}></input>
+        <br />
+        <br />
+
+        <label>Enter the description or additional notes of the task</label>
+          <br />
+          <br />
+        <input type = "text" name = "desc" value = {form.desc} onChange = {handleChange}></input>
+          <br />
+          <br />
+          <button type = "submit">Add task</button>
+      </form>
+      <div> 
+        {
+          task.map((item , index) =>(
+            <ol key = {index}>
+              <li>Name : {item.name}</li>
+              <li>Desc : {item.desc}</li>
+            </ol>
+          ))
+        }
+      </div>
+      </>
+    )
+  
 }
